5f8951ec3d515ba6398a4cc1038edbca56c2d66f
Banorant PC <keirtzyaldwinsosa@gmail.com>
2025-11-01 17:37:33 +0800

    ...

--- smart-contracts/ignition/modules/CertificateRegistryModule.js
+++ smart-contracts/ignition/modules/CertificateRegistryModule.js
@@ -1,10 +1,10 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
+
 const CertificateRegistryModule = buildModule("CertificateRegistryModule", (m) => {
   // Deploy the CertificateRegistry contract
   const certificateRegistry = m.contract("CertificateRegistry");
 
-  // Export the deployed contract instance
   return { certificateRegistry };
 });
 
