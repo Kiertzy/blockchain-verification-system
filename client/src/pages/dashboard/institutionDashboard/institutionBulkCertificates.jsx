3e0b84744d52c4a8a2c9041db3ad3bb8d54bea16
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-12-02 18:56:08 +0800

    ...

--- client/src/pages/dashboard/institutionDashboard/institutionBulkCertificates.jsx
+++ client/src/pages/dashboard/institutionDashboard/institutionBulkCertificates.jsx
@@ -33,7 +33,7 @@ const InstitutionBulkCertificates = () => {
     college: "",
     course: "",
     major: "",
-    certStatus: "ACTIVE",
+    certStatus: "CONFIRMED",
     walletAddressInstitution: "",
     dateIssued: new Date().toISOString().split("T")[0],
   });
